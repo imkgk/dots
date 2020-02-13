a760f2b59cad39888fa9d38616b33e2750d94095
imkgk <lnz013@gmail.com>
2020-02-13 10:23:40 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,29 +1,11 @@
 echo
 echo ===============
-echo installing tools
-echo sudo apt-get install -y build-essential git
-echo ===============
-echo
-
-sudo apt-get install -y build-essential git
-
-echo
-echo ===============
-echo installing vim, tmux
-echo sudo apt-get -y install vim vim-nox tmux
-echo ===============
-echo
-
-sudo apt-get -y install vim vim-nox tmux
-
-echo
-echo ===============
 echo clone dot files
-echo git clone https://github.com/markgeek/dots.git ~/.dots
+echo git clone https://github.com/imkgk/dots.git ~/.dots
 echo ===============
 echo
 
-git clone https://github.com/markgeek/dots.git ~/.dots
+git clone https://github.com/imkgk/dots.git ~/.dots
 
 echo
 echo ===============
@@ -55,14 +37,14 @@ ln -s ~/.dots/tmux.conf ~/.tmux.conf
 echo
 echo ===============
 echo clone and setup vim configuration
-echo git clone https://github.com/markgeek/vimfiles.git ~/.vim
+echo git clone https://github.com/imkgk/vimfiles.git ~/.vim
 echo git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 echo ln -s ~/.vim/vimrc ~/.vimrc
 echo vim +PluginInstall +qall
 echo ===============
 echo
 
-git clone https://github.com/markgeek/vimfiles.git ~/.vim
+git clone https://github.com/imkgk/vimfiles.git ~/.vim
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 ln -s ~/.vim/vimrc ~/.vimrc
 vim +PluginInstall +qall
