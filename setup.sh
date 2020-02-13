5e77d4ff9f061d268d4b218850ef443aab9f8db0
mkgk <mkgk@mkgkdeMacBook-Pro.local>
2020-02-13 15:20:08 +0800

    setup

---
+++ setup.sh
@@ -0,0 +1,41 @@
+echo
+echo ===============
+echo link gitconfig
+echo ln -s ~/.dots/gitconfig ~/.gitconfig
+echo ===============
+echo
+
+ln -s ~/.dots/gitconfig ~/.gitconfig
+
+echo
+echo ===============
+echo link gemrc
+echo ln -s ~/.dots/gemrc ~/.gemrc
+echo ===============
+echo
+
+ln -s ~/.dots/gemrc ~/.gemrc
+
+echo
+echo ===============
+echo link tmux.conf
+echo ln -s ~/.dots/tmux.conf ~/.tmux.conf
+echo ===============
+echo
+
+ln -s ~/.dots/tmux.conf ~/.tmux.conf
+
+echo
+echo ===============
+echo clone and setup vim configuration
+echo git clone https://github.com/imkgk/vimfiles.git ~/.vim
+echo git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+echo ln -s ~/.vim/vimrc ~/.vimrc
+echo vim +PluginInstall +qall
+echo ===============
+echo
+
+git clone https://github.com/imkgk/vimfiles.git ~/.vim
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+ln -s ~/.vim/vimrc ~/.vimrc
+vim +PluginInstall +qall
