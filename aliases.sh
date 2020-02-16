35223fec9d138dcfd7b946786cf22fb870d0d303
imkgk <lnz013@gmail.com>
2020-02-16 12:18:41 +0800

    Update aliases.sh

--- aliases.sh
+++ aliases.sh
@@ -5,11 +5,8 @@ export EDITOR=/usr/bin/vi
 
 alias ll='ls -la'
 alias lls='ls -l'
-alias wp='cd ~/Workspace'
 
-alias rs='rails s'
-alias rc='rails c'
-alias rg='rails g'
+alias wp='cd ~/Workspace'
 
 alias bd='bundle'
 alias bx='bundle exec'
@@ -18,9 +15,3 @@ alias bx='bundle exec'
 alias tm='tmux -2'
 alias tmls='tm ls'
 alias tmat='tm a -t'
-
-# for vagrant
-alias vg='vagrant'
-
-# for gradle
-alias g='./gradlew'
